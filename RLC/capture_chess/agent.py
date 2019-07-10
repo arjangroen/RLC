7b42b724d9f28501d33758c709bae68f8aed4996
arjangroen <arjanmartengroen@gmail.com>
2019-07-10 13:42:25 +0200

    :construction: Legal action selection in cnn

--- RLC/capture_chess/agent.py
+++ RLC/capture_chess/agent.py
@@ -9,7 +9,6 @@ def policy_gradient_loss(Returns):
     def modified_crossentropy(action,action_probs):
         #action_probs = action * action_probs
         cost = (K.categorical_crossentropy(action,action_probs,from_logits=False,axis=1) * Returns)
-        print(cost)
         return -cost
     return modified_crossentropy
 
@@ -94,11 +93,10 @@ class Agent(object):
         Returns:
 
         """
-
-
         optimizer = SGD(lr=self.lr, momentum=0.0, decay=0.0, nesterov=False)
         input_layer = Input(shape=(8, 8, 8), name='board_layer')
         R = Input(shape=(1,),name='Rewards')
+        legal_moves = Input(shape=4096,name='legal_move_mask')
         #true_action = Input(shape=(4096,),name='action_taken')
         inter_layer_1 = Conv2D(1, (1, 1), data_format="channels_first")(input_layer)  # 1,8,8
         inter_layer_2 = Conv2D(1, (1, 1), data_format="channels_first")(input_layer)  # 1,8,8
@@ -106,8 +104,9 @@ class Agent(object):
         flat_2 = Reshape(target_shape=(1, 64))(inter_layer_2)
         output_dot_layer = Dot(axes=1)([flat_1, flat_2])
         output_layer = Reshape(target_shape=(4096,))(output_dot_layer)
-        output_layer = Activation('softmax')(output_layer)
-        self.model = Model(inputs=[input_layer,R], outputs=[output_layer])
+        legal_output_layer = legal_moves * output_layer  # Select legal moves
+        softmax_layer = Activation('softmax')(legal_output_layer)
+        self.model = Model(inputs=[input_layer,R], outputs=[softmax_layer])
         #self.model.add_loss(policy_gradient_loss(true_action, output_layer, R))
         self.model.compile(optimizer=optimizer,loss=policy_gradient_loss(R))
 
