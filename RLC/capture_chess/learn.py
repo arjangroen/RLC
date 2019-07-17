851a3ad4fe1b850bff3d5d1117bad4a5a23ad9c0
arjangroen <arjanmartengroen@gmail.com>
2019-07-17 21:30:03 +0200

    :construction: adding actor critic

--- RLC/capture_chess/learn.py
+++ RLC/capture_chess/learn.py
@@ -263,4 +263,117 @@ class Reinforce(object):
         """
         self.agent.policy_gradient_update(states, actions,rewards, action_spaces)
 
+
+class ActorCritic(object):
+
+    def __init__(self, agent, env):
+        """
+        ActorCritic object to learn capture chess
+        Args:
+            agent: The agent playing the chess game as white
+            env: The environment including the python-chess board
+            memsize: maximum amount of games to retain in-memory
+        """
+        self.agent = agent
+        self.env = env
+        self.reward_trace = []
+        self.action_value_mem = []
+
+    def learn(self, iters=100, c=10):
+        """
+        Run the Q-learning algorithm. Play greedy on the final iter
+        Args:
+            iters: int
+                amount of games to train
+            c: int
+                update the network every c games
+
+        Returns: pgn (str)
+            pgn string describing final game
+
+        """
+        for k in range(iters):
+            self.env.reset()
+            states, actions, rewards, action_spaces = self.play_game(k)
+            self.reinforce_agent(states,actions, rewards, action_spaces)
+
+
+        pgn = Game.from_board(self.env.board)
+        reward_smooth = pd.DataFrame(self.reward_trace)
+        reward_smooth.rolling(window=10, min_periods=0).mean().plot()
+
+        return pgn
+
+    def play_game(self, k, maxiter=25):
+        """
+        Play a game of capture chess
+        Args:
+            k: int
+                game count, determines epsilon (exploration rate)
+            greedy: Boolean
+                if greedy, no exploration is done
+            maxiter: int
+                Maximum amount of steps per game
+
+        Returns:
+
+        """
+        episode_end = False
+        turncount = 0
+
+        states = []
+        actions = []
+        rewards = []
+        action_spaces = []
+
+        # Play a game of chess
+        while not episode_end:
+            state = self.env.layer_board
+            action_space = self.env.project_legal_moves()  # The environment determines which moves are legal
+            action_probs = self.agent.model.predict([np.expand_dims(state, axis=0),
+                                                     np.zeros((1,1)),
+                                                     action_space.reshape(1,4096)])
+            self.action_value_mem.append(action_probs)
+            action_probs = action_probs/action_probs.sum()
+            move = np.random.choice(range(4096),p=np.squeeze(action_probs))
+            move_from = move // 64
+            move_to = move % 64
+            moves = [x for x in self.env.board.generate_legal_moves() if \
+                     x.from_square == move_from and x.to_square == move_to]
+            assert len(moves) > 0  # should not be possible
+            if len(moves) > 1:
+                move = np.random.choice(moves)  # If there are multiple max-moves, pick a random one.
+            elif len(moves) == 1:
+                move = moves[0]
+
+            episode_end, reward = self.env.step(move)
+            new_state = self.env.layer_board
+            turncount += 1
+            if turncount > maxiter:
+                episode_end = True
+                reward = 0
+            if episode_end:
+                new_state = new_state * 0
+
+            states.append(state)
+            actions.append((move_from,move_to))
+            rewards.append(reward)
+            action_spaces.append(action_space.reshape(1,4096))
+
+        self.reward_trace.append(np.sum(rewards))
+
+        return states, actions, rewards, action_spaces
+
+
+    def reinforce_agent(self, states, actions, rewards, action_spaces):
+        """
+        Update the agent using experience replay. Set the sampling probs with the td error
+        Args:
+            turncount: int
+                Amount of turns played. Only sample the memory of there are sufficient samples
+        Returns:
+
+        """
+        self.agent.policy_gradient_update(states, actions,rewards, action_spaces)
+
         
