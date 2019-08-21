8f19d7a6f249168493aafe8bff3d610b9978057b
Arjan Groen <arjanmartengroen@gmail.com>
2019-08-21 18:05:04 +0200

    :construction: update board when selecting child

--- RLC/real_chess/tree.py
+++ RLC/real_chess/tree.py
@@ -62,6 +62,7 @@ class Node(object):
                     max_upper = child.upper_bound
                     max_move = move
             if max_move:
+                self.board.push(max_move)
                 return self.children[max_move]
             else:
                 return self
