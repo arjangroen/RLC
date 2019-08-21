0eb9a0e4d791acb9af08db7bcbbe973e9cbd05ea
Arjan Groen <arjanmartengroen@gmail.com>
2019-08-21 17:25:17 +0200

    :package: add real_chess

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
     name='RLC',
-    version='0.1',
-    packages=['RLC', 'RLC.move_chess', 'RLC.capture_chess'],
+    version='0.3',
+    packages=['RLC', 'RLC.move_chess', 'RLC.capture_chess', 'RLC.real_chess'],
     url='https://github.com/arjangroen/RLC',
     license='MIT',
     author='a.groen',
