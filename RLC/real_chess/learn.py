cdbf1585cb07d0528e5795ccb9aaed39a8d9d51b
arjangroen <arjanmartengroen@gmail.com>
2019-09-13 22:40:10 +0200

    :pencil: typo in network

--- RLC/real_chess/learn.py
+++ RLC/real_chess/learn.py
@@ -148,6 +148,7 @@ class TD_search(object):
         print("game ended with result",reward, "and material balance",piece_balance, "in",turncount,"halfmoves")
         if np.abs(reward) == 1:
             print(self.env.board)
+            print(self.env.layer_board[5])
 
 
         return self.env.board
