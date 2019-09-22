:bug: mcts backprop

RLC/real_chess/learn.py
@@ -65,6 +65,7 @@ class TD_search(object):
         episode_end = False
         turncount = 0
         tree = Node(self.env.board, gamma=self.gamma)
+        tree.values = [0]
 
         # Play a game of chess
         while not episode_end:
@@ -119,11 +120,17 @@ class TD_search(object):
             tree = tree.children[max_move]
             tree.parent = None
 
+            print(tree.values)
+
             sucstate = np.expand_dims(self.env.layer_board, axis=0)
             new_state_value = self.agent.predict(sucstate)
 
+            print(new_state_value.item())
+
             if not tree.values:
-                tree.values = [new_state_value.item()]
+                tree.values.append(new_state_value.item())
+
+            print(tree.values)
 
             error = reward + self.gamma * new_state_value - state_value
             error = np.float(np.squeeze(error))

RLC/real_chess/tree.py
@@ -31,14 +31,14 @@ class Node(object):
     def backprop(self, result):
         self.parent.values.append(self.gamma*result)
 
-    def select(self,color=1):
+    def select(self, color=1):
         """Thompson sampling"""
         assert color == 1 or color == -1, "color has to be white (1) or black (-1)"
         if self.children:
-            max_sample = np.max(color * self.values)
+            max_sample = np.max(color * np.array(self.values))
             max_move = None
             for move, child in self.children.items():
-                child_sample = np.max(color * child.values)
+                child_sample = np.max(color * np.array(child.values))
                 if child_sample > max_sample:
                     max_sample = child_sample
                     max_move = move