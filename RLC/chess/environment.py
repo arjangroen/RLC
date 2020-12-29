ba01fdaf7efcf8fd0f24cc5a383902ebe8a6acf6
ArjanGroen <a.groen@quin.md>
2020-12-29 17:17:46 +0100

    :alembic: maintain tree

---
+++ RLC/chess/environment.py
@@ -0,0 +1,144 @@
+import chess
+import numpy as np
+
+mapper = {}
+mapper["p"] = 0
+mapper["r"] = 1
+mapper["n"] = 2
+mapper["b"] = 3
+mapper["q"] = 4
+mapper["k"] = 5
+mapper["P"] = 0
+mapper["R"] = 1
+mapper["N"] = 2
+mapper["B"] = 3
+mapper["Q"] = 4
+mapper["K"] = 5
+
+
+class Board(object):
+
+    def __init__(self, opposing_agent, FEN=None, capture_reward_factor=0.01):
+        """
+        Chess Board Environment
+        Args:
+            FEN: str
+                Starting FEN notation, if None then start in the default chess position
+            capture_reward_factor: float [0,inf]
+                reward for capturing a piece. Multiply material gain by this number. 0 for normal chess.
+        """
+        self.FEN = FEN
+        self.capture_reward_factor = capture_reward_factor
+        self.board = chess.Board(self.FEN) if self.FEN else chess.Board()
+        self.layer_board = np.zeros(shape=(8, 8, 8))
+        self.init_layer_board()
+        self.opposing_agent = opposing_agent
+
+    def init_layer_board(self):
+        """
+        Initalize the numerical representation of the environment
+        Returns:
+
+        """
+        self.layer_board = np.zeros(shape=(8, 8, 8))
+        for i in range(64):
+            row = i // 8
+            col = i % 8
+            piece = self.board.piece_at(i)
+            if piece == None:
+                continue
+            elif piece.symbol().isupper():
+                sign = 1
+            else:
+                sign = -1
+            layer = mapper[piece.symbol()]
+            self.layer_board[layer, row, col] = sign
+            self.layer_board[6, :, :] = 1 / self.board.fullmove_number
+        if self.board.turn:
+            self.layer_board[6, 0, :] = 1
+        else:
+            self.layer_board[6, 0, :] = -1
+        self.layer_board[7, :, :] = 1
+
+    def update_layer_board(self, move=None):
+        self._prev_layer_board = self.layer_board.copy()
+        self.init_layer_board()
+
+    def pop_layer_board(self):
+        self.layer_board = self._prev_layer_board.copy()
+        self._prev_layer_board = None
+
+    def step(self, action):
+        """
+        Run a step
+        Args:
+            action: python chess move
+        Returns:
+            epsiode end: Boolean
+                Whether the episode has ended
+            reward: float
+                Difference in material value after the move
+        """
+        piece_balance_before = self.get_material_value()
+        self.board.push(action)
+        self.update_layer_board(action)
+        piece_balance_after = self.get_material_value()
+        auxiliary_reward = (piece_balance_after - piece_balance_before) * self.capture_reward_factor
+        result = self.board.result()
+        if result == "*":
+            reward = 0
+            episode_end = False
+        elif result == "1-0":
+            reward = 1
+            episode_end = True
+        elif result == "0-1":
+            reward = -1
+            episode_end = True
+        elif result == "1/2-1/2":
+            reward = 0
+            episode_end = True
+        reward += auxiliary_reward
+
+        return episode_end, reward
+
+    def get_random_action(self):
+        """
+        Sample a random action
+        Returns: move
+            A legal python chess move.
+
+        """
+        legal_moves = [x for x in self.board.generate_legal_moves()]
+        legal_moves = np.random.choice(legal_moves)
+        return legal_moves
+
+    def project_legal_moves(self):
+        """
+        Create a mask of legal actions
+        Returns: np.ndarray with shape (64,64)
+        """
+        self.action_space = np.zeros(shape=(64, 64))
+        moves = [[x.from_square, x.to_square] for x in self.board.generate_legal_moves()]
+        for move in moves:
+            self.action_space[move[0], move[1]] = 1
+        return self.action_space
+
+    def get_material_value(self):
+        """
+        Sums up the material balance using Reinfield values
+        Returns: The material balance on the board
+        """
+        pawns = 1 * np.sum(self.layer_board[0, :, :])
+        rooks = 5 * np.sum(self.layer_board[1, :, :])
+        minor = 3 * np.sum(self.layer_board[2:4, :, :])
+        queen = 9 * np.sum(self.layer_board[4, :, :])
+        return pawns + rooks + minor + queen
+
+    def reset(self):
+        """
+        Reset the environment
+        Returns:
+
+        """
+        self.board = chess.Board(self.FEN) if self.FEN else chess.Board()
+        self.init_layer_board()
---
+++ RLC/chess/montecarlo.py
@@ -0,0 +1,33 @@
+class PlayOut(object):
+
+    def __init__(self, gamma=0.9):
+        self.gamma = gamma
+
+    def sim(self, env, white, black, depth=0):
+        """
+        Recursive playout simulation
+        :param env: environment
+        :param white: white agent
+        :param black: black agent
+        :param depth: current depth
+        :return: Returns
+        """
+
+        successor_values = []
+        moves = []
+        for move in env.board.generate_legal_moves():
+            current_player = white if env.board.turn else black
+            successor_values.append(current_player.evaluate(move, env))
+            moves.append(move)
+        selected_move = current_player.select(moves, successor_values)
+        episode_end, reward = env.step(selected_move)
+
+        if episode_end:
+            return reward
+        else:
+            returns = reward + self.gamma * self.sim(env, white, black, depth=depth + 1)
+
+        env.pull_move()
+        return returns
+
+
